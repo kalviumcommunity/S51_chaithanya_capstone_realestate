fb9ea656c13117af0643c7ccdd206e2c89cb8fd4
chaithanyachandrachud <chaithanya.chud@gmail.com>
2024-03-14 14:30:41 +0530

    donee

---
+++ client/real/src/components/SignUp.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SignUp = () => {
+  return (
+    <div>
+      <h1>Sign Up Page</h1>
+      <p>Create an account...</p>
+    </div>
+  );
+};
+
+export default SignUp;
