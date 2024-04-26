3a6e1594e85192c61a147fd49d8c4b069c2e1b9f
chaithanyachandrachud <chaithanya.chud@gmail.com>
2024-04-26 16:50:57 +0530

    whatsapp added

--- backend/Routes/routes.js
+++ backend/Routes/routes.js
@@ -1,25 +1,25 @@
-bconst express = require("express")
+const express = require("express")
 
-const router = express.router()
+const router = express.Router()
 const user = require("../Models/User")
 
-router.post("/login", (req, res) => {
+router.post("/login", async (req, res) => {
     const { email, password } = req.body;
 
     try {
 
-        const user = awaitLogin.findOne({ email });
+        const user = await Login.findOne({ email });
         if (!user) {
             return res.status(404).json({ message: 'User not found' });
         }
 
-      
+
         const passwordMatch = awaitbcrypt.compare(password, user.password);
         if (!passwordMatch) {
             return res.status(401).json({ message: 'Invalid password' });
         }
 
-     
+
         const token = jwt.sign({ email }, process.env.SECRET_TOKEN);
         res.json({ token });
     } catch (error) {
@@ -28,3 +28,25 @@ router.post("/login", (req, res) => {
     }
 
 })
+
+router.post("/signup", async (req, res) => {
+    const { password, email, username } = req.body;
+
+    try {
+        const newuser = user.create({
+            password,
+            email,
+            username
+        })
+        console.log(newuser)
+        res.status(200).json(newuser)
+
+
+
+
+    } catch (error) {
+        console.log(error.message)
+    }
+})
+
+module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 require('dotenv').config()
-const {startDatabase,isConnected} = require('./db')
+const { startDatabase, isConnected } = require('./db')
 const app = express();
 const port = 3000;
-const cors=require('cors');
-const FeedbackRouter = require("./Routes/Feedback.route")
+const cors = require('cors');
+const FeedbackRouter = require("./Routes/Feedback.route");
+const { router } = require("./Routes/routes");
 
 app.use(cors())
 app.get('/', (req, res) => {
@@ -15,7 +16,7 @@ app.get('/', (req, res) => {
 });
 app.use(express.json())
 app.use(FeedbackRouter)
-
+app.use("/api/auth", router)
 
 app.listen(port, async () => {
   await startDatabase();
